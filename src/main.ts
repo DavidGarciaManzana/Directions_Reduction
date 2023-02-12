b6a9488e94ae8f5ee1e46028f46efe3db700d21c
David Garcia <Davidgarciama@outlook.com>
2023-02-12 14:34:37 -0600

    Make code readable and easily maintainable

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,49 @@
-export function dirReduc(arr: string[]): string[] {
-
-    function arrayRemove(array: string[], index: number) {
-        
-        return array.splice(index, 1)
-    }
-
-    let removeConsecutiveMoves = (array: string[], lastDirection: string): string[] | void => {
-        
-        for (let i = 0; i < array.length; i++) {
-            if (lastDirection == "NORTH" && arr[i] == "SOUTH") {
-                
-                arrayRemove(array, i)
-                arrayRemove(array, i - 1)
-                // console.log(array)
-                return array
-            } else if (lastDirection == "SOUTH" && arr[i] == "NORTH") {
-                arrayRemove(array, i)
-                arrayRemove(array, i - 1)
-                // console.log(array)
-                return array
-            } else if (lastDirection == "EAST" && arr[i] == "WEST") {
-                arrayRemove(array, i)
-                arrayRemove(array, i - 1)
-                // console.log(array)
-                return array
-            } else if (lastDirection == "WEST" && arr[i] == "EAST") {
-                arrayRemove(array, i)
-                arrayRemove(array, i - 1)
-                // console.log(array)
-                return array
-            }
-            lastDirection = array[i]
-        }
-
-
-    }
-
-
-
-    const contariesConsecutiveWordsExists = (arr: string[]) => {
-        let lastDirection = arr[0]
+const dirReduc = (arr: string[]): string[] => {
+    const contrariesConsecutiveWordsExists = (arr: string[]): boolean => {
+        let lastDirection = arr[0];
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] == "SOUTH" && lastDirection == "NORTH" || arr[i] == "NORTH" && lastDirection == "SOUTH" || arr[i] == "EAST" && lastDirection == "WEST" || arr[i] == "WEST" && lastDirection == "EAST") {
-                return true
-            }
-            lastDirection = arr[i]
+                return true;
+            };
+            lastDirection = arr[i];
         }
-        return false
+        return false;
     }
 
-    while (contariesConsecutiveWordsExists(arr)) {
-        removeConsecutiveMoves(arr, arr[0])
+    const arrayRemove = (array: string[], index: number) => {
+        return array.splice(index, 1);
     }
 
+    const removeFirstTwoConsecutiveMoves = (array: string[]): string[] | void => {
+        let lastDirection = array[0]
+        for (let i = 0; i < array.length; i++) {
+            if (lastDirection == "NORTH" && arr[i] == "SOUTH") {
+                arrayRemove(array, i);
+                arrayRemove(array, i - 1);
+                return array;
+            } else if (lastDirection == "SOUTH" && arr[i] == "NORTH") {
+                arrayRemove(array, i);
+                arrayRemove(array, i - 1);
+                return array;
+            } else if (lastDirection == "EAST" && arr[i] == "WEST") {
+                arrayRemove(array, i);
+                arrayRemove(array, i - 1);
+                return array;
+            } else if (lastDirection == "WEST" && arr[i] == "EAST") {
+                arrayRemove(array, i);
+                arrayRemove(array, i - 1);
+                return array;
+            }
+            lastDirection = array[i];
+        }
+    }
+
+    
+    //1.-Make sure there are words to remove and look after remove other words
+    while (contrariesConsecutiveWordsExists(arr)) {
+        //2.-Remove the first two contrary words 
+        removeFirstTwoConsecutiveMoves(arr)
+    };
     return arr;
 }
 console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))//, ["WEST"]);
